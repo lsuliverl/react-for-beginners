ed94f4ed32a5f3ae47eb8c0fdfccb11da91b87b2
lsuliverl <sh000803@naver.com>
2024-10-21 15:41:25 +0900

    useEffect 사용해봄

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setValue] = useState(0);
+    const onClick = () => setValue((prev) => prev + 1);
+    console.log("난 항상 실행돼");
+    const iRunOnlyOnce = () => {
+        console.log("난 한번만 실행돼");
+    };
+    useEffect(iRunOnlyOnce, []);
     return (
         <div>
-            <h1 className={styles.title}>안녕?</h1>
+            <h1 className={styles.title}>{counter}</h1>
             <Button text={"계속"} />
+            <button onClick={onClick}>클릭</button>
         </div>
     );
 }
