6cd98781e7e9eb83375b1966076fdba00a6b7062
lsuliverl <sh000803@naver.com>
2024-10-31 16:03:19 +0900

    검색기능 useEffect 사용해서 만들어봄

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import { useState, useEffect } from "react";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
+    const onChange = (event) => setKeyword(event.target.value);
     console.log("난 항상 실행돼");
     useEffect(() => {
         console.log("api 불러오기");
     }, []);
+    useEffect(() => {
+        if (keyword !== "" && keyword.length > 5) {
+            console.log("검색 결과", keyword);
+        }
+    }, [keyword]);
     return (
         <div>
+            <input value={keyword} onChange={onChange} type="text" placeholder="여기에 검색해주세요..." />
             <h1 className={styles.title}>{counter}</h1>
             <Button text={"계속"} />
             <button onClick={onClick}>클릭</button>
